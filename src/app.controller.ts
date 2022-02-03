9bfbe4be0ed546900402463e76e4e130c0d997ad
alexandresantosm <amenezes031@gmail.com>
2022-02-03 18:57:03 -0300

    move endpiont POST /products to your file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('greetings')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('products')
-  getPublishedProducts() {
-    return this.appService.findPublishedProducts();
   }
 }
