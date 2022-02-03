cf8ea9920ce0208fdb4cdd5f31d9f5b01250d3bd
alexandresantosm <amenezes031@gmail.com>
2022-02-03 17:18:24 -0300

    create method that returns all published products

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async findPublishedProducts() {
+    const publishedProducts = await this.prisma.product.findMany({
+      where: {
+        published: true,
+      },
+    });
+
+    return publishedProducts;
   }
 }
