e4adfcfc8cd3b70c474d4023b67b2991b935141c
alexandresantosm <amenezes031@gmail.com>
2022-02-03 16:28:27 -0300

    inicialize Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('NestJS Prisma')
+    .setDescription('Building REST API with NestJS and Prisma')
+    .setVersion('1.0')
+    .build();
+  const documentSwagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, documentSwagger);
+
   await app.listen(3000);
 }
 bootstrap();
