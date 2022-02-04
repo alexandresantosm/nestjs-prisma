adbc475f86eb81b4640c9c54cd188c60d5e3e737
alexandresantosm <amenezes031@gmail.com>
2022-02-03 21:12:14 -0300

    add annotate REST endpoints with the custom short-hand API response

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,7 +10,8 @@ import {
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ProductEntity } from './entities/product.entity';
 
 @Controller('products')
 @ApiTags('products')
@@ -18,31 +19,59 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'The product has been successfully created.',
+    type: ProductEntity,
+  })
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   @Get()
+  @ApiOkResponse({
+    description:
+      'The list of published products has been successfully returned.',
+    type: ProductEntity,
+    isArray: true,
+  })
   findAll() {
     return this.productsService.findAll();
   }
 
   @Get('/drafts')
+  @ApiOkResponse({
+    description:
+      'The list of products not published has been successfully returned.',
+    type: ProductEntity,
+    isArray: true,
+  })
   findAllDrafts() {
     return this.productsService.findAllDrafts();
   }
 
   @Get(':id')
+  @ApiOkResponse({
+    description: 'The product has been successfully returned.',
+    type: ProductEntity,
+  })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({
+    description: 'A product has been successfully updated.',
+    type: ProductEntity,
+  })
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({
+    description: 'A product has been successfully deleted.',
+    type: ProductEntity,
+  })
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
   }
