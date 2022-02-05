6f010748d7676a316dacccb3a90bfc4493faa9bb
alexandresantosm <amenezes031@gmail.com>
2022-02-05 15:04:09 -0300

    apply handle exceptions

--- src/exceptions/prisma-client-exception.filter.ts
+++ src/exceptions/prisma-client-exception.filter.ts
@@ -1,23 +1,29 @@
-import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { ArgumentsHost, Catch } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
 import { Response } from 'express';
+import { PrismaErrorCode } from '../constants/prismaErrorCode';
+import {
+  exceptionRecordNotFound,
+  exceptionUniqueConstraintFailed,
+  exceptionValueTooLong,
+} from './handleExceptions';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const context = host.switchToHttp();
     const response = context.getResponse<Response>();
-    const prismaErrorCodeUniqueConstraintFailed = 'P2002';
 
     switch (exception.code) {
-      case prismaErrorCodeUniqueConstraintFailed:
-        const status = HttpStatus.CONFLICT;
-        const message = exception.message.replace(/\n/g, '');
-        response.status(status).json({
-          statusCode: status,
-          message,
-        });
+      case PrismaErrorCode.UNIQUE_CONSTRAINT_FAILED:
+        exceptionUniqueConstraintFailed(exception, response);
+        break;
+      case PrismaErrorCode.VALUE_TOO_LONG:
+        exceptionValueTooLong(exception, response);
+        break;
+      case PrismaErrorCode.RECORD_NOT_FOUND:
+        exceptionRecordNotFound(exception, response);
         break;
       default:
         super.catch(exception, host);
