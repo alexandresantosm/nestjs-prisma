fc1162c00a58b71bde66462b8034cd2d4f55657a
alexandresantosm <amenezes031@gmail.com>
2022-02-05 11:05:38 -0300

    create a new response with a status code of Conflit and return the exception message

--- src/prisma-client-exception.filter.ts
+++ src/prisma-client-exception.filter.ts
@@ -1,10 +1,27 @@
-import { ArgumentsHost, Catch } from '@nestjs/common';
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
+import { Response } from 'express';
 
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
-    super.catch(exception, host);
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const prismaErrorCodeUniqueConstraintFailed = 'P2002';
+
+    switch (exception.code) {
+      case prismaErrorCodeUniqueConstraintFailed:
+        const status = HttpStatus.CONFLICT;
+        const message = exception.message.replace(/\n/g, '');
+        response.status(status).json({
+          statusCode: status,
+          message,
+        });
+        break;
+      default:
+        super.catch(exception, host);
+        break;
+    }
   }
 }
