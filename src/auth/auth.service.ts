6e7241d04571e557121e7b1f7791b239fe67b284
alexandresantosm <amenezes031@gmail.com>
2022-02-09 12:03:10 -0300

    impement login logic in the AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,36 @@
+import { NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/login.dto';
+import { Auth } from './entities/auth.entity';
 
 @Injectable()
 export class AuthService {
-  async login(loginDto: LoginDto) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginDto: LoginDto): Promise<Auth> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: loginDto.email,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`No user found for email: ${loginDto.email}`);
+    }
+
+    const passwordValid = user.password === loginDto.password;
+
+    if (!passwordValid) {
+      throw new UnauthorizedException('Invalid password');
+    }
+
+    const accessToken = this.jwtService.sign({ userId: user.id });
+
+    return { accessToken };
+  }
 }
